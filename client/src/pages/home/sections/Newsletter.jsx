519c88e0c9827d34378a02ef9a7bcdb0cef842ef
Ariyan Rahman Anas <anas.hllw@gmail.com>
2025-01-21 08:47:36 +0600

    designed news letter ui in home page

--- client/src/pages/home/sections/Newsletter.jsx
+++ client/src/pages/home/sections/Newsletter.jsx
@@ -1,11 +1,12 @@
 import { useForm } from "react-hook-form"
 import newsLetterImg from "./../../../assets/images/home/newsLetter.webp"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Loader2 } from "lucide-react"
+import { Loader2, Send } from "lucide-react"
+import { Card } from "@/components/ui/card"
 
 const Newsletter = () => {
 
@@ -18,6 +19,20 @@ const Newsletter = () => {
     setError
   } = useForm()
 
+  const user = useSelector(state => state?.auth?.user)
+
+  const handleSubscribe = (formData) => {
+    console.log({ formData })
+  }
+
+
+  const newsLetterFacilities = [
+    { id: 1, title: "Early access to sales." },
+    { id: 2, title: "Exclusive discounts." },
+    { id: 3, title: "Tips to style your home like a pro." },
+    { id: 4, title: "WellWood furniture news, and" },
+    { id: 5, title: "WellWood furniture events." },
+  ]
 
   return (
     <div className="space-y-4">
@@ -26,51 +41,67 @@ const Newsletter = () => {
         <p className="subheading" >Get exclusive offers, New arrivals, and style tips delivered to your inbox!</p>
       </div>
 
-      <div className="flex flex-col md:flex-row items-center justify-between gap-8">
-        <div>
-          <img src={newsLetterImg} alt="WellWood furniture" loading="lazy" className="rounded-l-3xl" />
-        </div>
-        <form
-          // onSubmit={handleSubmit(handleLogin)}
-          className="w-full"
-        >
-          <div className="grid gap-4">
-            <div className="grid gap-0.5">
-              <Label htmlFor="email">Email
-                <span className="text-myRed text-lg">*</span>
-              </Label>
-              <Input
-                {...register("email", { required: true })}
-                id="email"
-                name="email"
-                type="email"
-                placeholder="name@example.com"
-              />
-            </div>
+      <div className="w-full h-full">
+        <img src={newsLetterImg} alt="WellWood furniture" loading="lazy" className="rounded-3xl w-full h-full" />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
 
-            <div className="grid gap-0.5">
-              <Label htmlFor="password">Password
-                <span className="text-myRed text-lg">*</span>
-              </Label>
-              <Input
-                {...register("password", {
-                  required: true,
-                  minLength: {
-                    value: 6,
-                    message: "Password must be at least 6 characters",
-                  },
-                })}
-                id="password"
-                name="password"
-                type="password"
-                placeholder="********"
-              />
-            </div>
+        <Card className="p-4">
+          <h1 className="poppins-bold text-2xl">Be Inspired Every Month!</h1>
+          <p className="mt-4 mb-2 text-lg text-gray-500">Sign up for our newsletter and enjoy:</p>
+          <ul className="list-disc ">
+            {newsLetterFacilities.map(({ id, title }) => (
+              <li key={id} className="list-inside">
+                {title}
+              </li>
+            ))}
+          </ul>
 
-            <Button
-              // disabled={isLoading}
-            >
-              {/* {
+
+        </Card>
+
+        <Card className="p-4">
+          <Send size={50} className="w-fit mx-auto animate-bounce " />
+
+          <form
+            onSubmit={handleSubmit(handleSubscribe)}
+            className="w-full"
+          >
+            <div className="grid gap-4">
+
+              <div className="grid gap-.5">
+                <Label htmlFor="name">Name
+                  <span className="text-myRed text-lg">*</span>
+                </Label>
+                <Input
+                  {...register("name", { required: true })}
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="John Smith"
+                  defaultValue={user?.name}
+                />
+              </div>
+
+              <div className="grid gap-0.5">
+                <Label htmlFor="email">Email
+                  <span className="text-myRed text-lg">*</span>
+                </Label>
+                <Input
+                  {...register("email", { required: true })}
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="name@example.com"
+                  defaultValue={user?.email}
+                />
+              </div>
+
+              <Button
+                // disabled={isLoading}
+                className="w-fit mx-auto"
+              >
+                {/* {
                 isLoading ? (
                   <>
                     Please wait
@@ -78,10 +109,11 @@ const Newsletter = () => {
                   </>
                 ) : "Login"
               } */}
-              Sign Up Now
-            </Button>
-          </div>
-        </form>
+                Subscribe Now
+              </Button>
+            </div>
+          </form>
+        </Card>
       </div>
 
     </div>
